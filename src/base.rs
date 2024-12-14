28914aae8be5f849f47202e4c76ebbbbac9db5da
Vašek Potoček <vasek.potocek@post.cz>
2024-12-14 14:23:57 +0100

    Refactor: split files

---
+++ src/base.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, PartialEq)]
+pub enum Object {
+    Bool(bool),
+    Number(Number),
+    String(Vec<u8>),
+    Name(Name),
+    Array(Vec<Object>),
+    Dict(Vec<(Name, Object)>),
+    Stream(Vec<(Name, Object)>, Vec<u8>),
+    Null
+}
+
+impl Object {
+    pub fn new_string(s: &str) -> Object {
+        Object::String(s.bytes().collect())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Number {
+    Int(i64),
+    Real(f64)
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Name(Vec<u8>);
