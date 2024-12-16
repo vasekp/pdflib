7ca510aab8150be4b50005bca04dfed22ed227c2
Vašek Potoček <vasek.potocek@post.cz>
2024-12-16 12:04:38 +0100

    Split Tokenizer from ByteIterator

--- src/parser.rs
+++ src/parser.rs
@@ -23,6 +23,13 @@ impl CharClass {
 }
 
 
+trait ByteProvider {
+    fn next_or_eof(&mut self) -> std::io::Result<u8>;
+    fn next_if(&mut self, cond: impl FnOnce(u8) -> bool) -> Option<u8>;
+    fn peek(&mut self) -> Option<u8>;
+}
+
+
 struct ByteIterator<T: Iterator<Item = std::io::Result<u8>>>(Peekable<T>);
 
 impl<T: Iterator<Item = std::io::Result<u8>>> From<T> for ByteIterator<T> {
@@ -37,7 +44,7 @@ impl From<&str> for ByteIterator<std::io::Bytes<Cursor<String>>> {
     }
 }
 
-impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
+impl<T: Iterator<Item = std::io::Result<u8>>> ByteProvider for ByteIterator<T> {
     fn next_or_eof(&mut self) -> std::io::Result<u8> {
         self.0.next().ok_or(std::io::Error::from(std::io::ErrorKind::UnexpectedEof))?
     }
@@ -54,28 +61,35 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
             _ => None
         }
     }
+}
 
+
+struct Tokenizer<T: ByteProvider> {
+    iter: T
+}
+
+impl<T: ByteProvider> Tokenizer<T> {
     fn read_token(&mut self) -> std::io::Result<Vec<u8>> {
-        let c = self.next_or_eof()?;
+        let c = self.iter.next_or_eof()?;
         match CharClass::of(c) {
             CharClass::Delim => {
-                if (c == b'<' || c == b'>') && self.next_if(|c2| c2 == c).is_some() {
+                if (c == b'<' || c == b'>') && self.iter.next_if(|c2| c2 == c).is_some() {
                     Ok([c, c].into())
                 } else if c == b'%' {
-                    while self.next_if(|c| c != b'\n' && c != b'\r').is_some() { }
+                    while self.iter.next_if(|c| c != b'\n' && c != b'\r').is_some() { }
                     Ok([b' '].into())
                 } else {
                     Ok([c].into())
                 }
             },
             CharClass::Space => {
-                while self.next_if(|c| CharClass::of(c) == CharClass::Space).is_some() { }
+                while self.iter.next_if(|c| CharClass::of(c) == CharClass::Space).is_some() { }
                 Ok([b' '].into())
             },
             CharClass::Reg => {
                 let mut ret = Vec::new();
                 ret.push(c);
-                while let Some(r) = self.next_if(|c| CharClass::of(c) == CharClass::Reg) {
+                while let Some(r) = self.iter.next_if(|c| CharClass::of(c) == CharClass::Reg) {
                     ret.push(r);
                 }
                 Ok(ret)
@@ -130,9 +144,9 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
         let mut ret = Vec::new();
         let mut parens = 0;
         loop {
-            match self.next_or_eof()? {
+            match self.iter.next_or_eof()? {
                 b'\\' => {
-                    let c = match self.next_or_eof()? {
+                    let c = match self.iter.next_or_eof()? {
                         b'n' => b'\x0a',
                         b'r' => b'\x0d',
                         b't' => b'\x09',
@@ -141,8 +155,8 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
                         c @ (b'(' | b')' | b'\\') => c,
                         d1 @ (b'0' ..= b'7') => {
                             let d1 = d1 - b'0';
-                            let d2 = self.next_if(|c| c >= b'0' && c <= b'7').map(|c| c - b'0');
-                            let d3 = self.next_if(|c| c >= b'0' && c <= b'7').map(|c| c - b'0');
+                            let d2 = self.iter.next_if(|c| c >= b'0' && c <= b'7').map(|c| c - b'0');
+                            let d3 = self.iter.next_if(|c| c >= b'0' && c <= b'7').map(|c| c - b'0');
                             match (d2, d3) {
                                 (Some(d2), Some(d3)) => (d1 << 6) + (d2 << 3) + d3,
                                 (Some(d2), None) => (d1 << 3) + d2,
@@ -155,7 +169,7 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
                     ret.push(c);
                 },
                 b'\r' => {
-                    self.next_if(|c| c == b'\n');
+                    self.iter.next_if(|c| c == b'\n');
                     ret.push(b'\n');
                 },
                 c => {
@@ -174,7 +188,7 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
         let mut msd = None;
         let mut ret = Vec::new();
         loop {
-            let c = self.next_or_eof()?;
+            let c = self.iter.next_or_eof()?;
             let dig = match c {
                 b'0'..=b'9' => c - b'0',
                 b'a'..=b'f' => c - b'a' + 10,
@@ -195,7 +209,7 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
     }
 
     fn read_name(&mut self) -> std::io::Result<Object> {
-        match self.peek() {
+        match self.iter.peek() {
             Some(c) if CharClass::of(c) != CharClass::Reg => return Ok(Object::Name(Name(Vec::new()))),
             None => return Ok(Object::Name(Name(Vec::new()))),
             _ => ()
@@ -230,6 +244,12 @@ impl<T: Iterator<Item = std::io::Result<u8>>> ByteIterator<T> {
     }
 }
 
+impl<T: Iterator<Item = std::io::Result<u8>>, U: Into<ByteIterator<T>>> From<U> for Tokenizer<ByteIterator<T>> {
+    fn from(iter: U) -> Self {
+        Tokenizer { iter: iter.into() }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -260,7 +280,7 @@ mod tests {
 
     #[test]
     fn test_tokenizer() {
-        let mut bytes = ByteIterator::from("abc  <<g,%k\r\nn");
+        let mut bytes = Tokenizer::from("abc  <<g,%k\r\nn");
         assert_eq!(bytes.read_token().unwrap(), b"abc");
         assert_eq!(bytes.read_token().unwrap(), b" ");
         assert_eq!(bytes.read_token().unwrap(), b"<<");
@@ -270,7 +290,7 @@ mod tests {
         assert_eq!(bytes.read_token().unwrap(), b"n");
         assert!(bytes.read_token().is_err());
 
-        let mut bytes = ByteIterator::from("A%1\rB%2\nC");
+        let mut bytes = Tokenizer::from("A%1\rB%2\nC");
         assert_eq!(bytes.read_token().unwrap(), b"A");
         assert_eq!(bytes.read_token().unwrap(), b" ");
         assert_eq!(bytes.read_token().unwrap(), b" ");
@@ -279,14 +299,14 @@ mod tests {
         assert_eq!(bytes.read_token().unwrap(), b" ");
         assert_eq!(bytes.read_token().unwrap(), b"C");
 
-        let mut bytes = ByteIterator::from("A%1\r %2\nB");
+        let mut bytes = Tokenizer::from("A%1\r %2\nB");
         assert_eq!(bytes.read_token_nonempty().unwrap(), b"A");
         assert_eq!(bytes.read_token_nonempty().unwrap(), b"B");
     }
 
     #[test]
     fn test_read_obj() {
-        let mut bytes = ByteIterator::from("true false 123 +17 -98 0 34.5 -3.62 +123.6 4. -.002 0.0");
+        let mut bytes = Tokenizer::from("true false 123 +17 -98 0 34.5 -3.62 +123.6 4. -.002 0.0");
         assert_eq!(bytes.read_obj().unwrap(), Object::Bool(true));
         assert_eq!(bytes.read_obj().unwrap(), Object::Bool(false));
         assert_eq!(bytes.read_obj().unwrap(), Object::Number(Number::Int(123)));
@@ -300,7 +320,7 @@ mod tests {
         assert_eq!(bytes.read_obj().unwrap(), Object::Number(Number::Real(-0.002)));
         assert_eq!(bytes.read_obj().unwrap(), Object::Number(Number::Real(0.)));
 
-        let mut bytes = ByteIterator::from("++1 1..0 .1. 1_ 1a 16#FFFE . 6.023E23 true");
+        let mut bytes = Tokenizer::from("++1 1..0 .1. 1_ 1a 16#FFFE . 6.023E23 true");
         assert!(bytes.read_obj().is_err());
         assert!(bytes.read_obj().is_err());
         assert!(bytes.read_obj().is_err());
@@ -314,7 +334,7 @@ mod tests {
 
     #[test]
     fn test_read_lit_string() {
-        let mut bytes = ByteIterator::from("(string) (new
+        let mut bytes = Tokenizer::from("(string) (new
 line) (parens() (*!&}^%etc).) () ((0)) (()");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("string"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("new\nline"));
@@ -323,26 +343,26 @@ line) (parens() (*!&}^%etc).) () ((0)) (()");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("(0)"));
         assert!(bytes.read_obj().is_err());
 
-        let mut bytes = ByteIterator::from("(These \\
+        let mut bytes = Tokenizer::from("(These \\
 two strings \\
 are the same.) (These two strings are the same.)");
         assert_eq!(bytes.read_obj().unwrap(), bytes.read_obj().unwrap());
 
-        let mut bytes = ByteIterator::from("(1
+        let mut bytes = Tokenizer::from("(1
 ) (2\\n) (3\\r) (4\\r\\n)");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("1\n"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("2\n"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("3\r"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("4\r\n"));
 
-        let mut bytes = ByteIterator::from("(1
+        let mut bytes = Tokenizer::from("(1
 ) (2\n) (3\r) (4\r\n)");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("1\n"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("2\n"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("3\n"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("4\n"));
 
-        let mut bytes = ByteIterator::from("(\\157cta\\154) (\\500) (\\0053\\053\\53) (\\53x)");
+        let mut bytes = Tokenizer::from("(\\157cta\\154) (\\500) (\\0053\\053\\53) (\\53x)");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("octal"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("@"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("\x053++"));
@@ -351,19 +371,19 @@ are the same.) (These two strings are the same.)");
 
     #[test]
     fn test_read_hex_string() {
-        let mut bytes = ByteIterator::from("<4E6F762073686D6F7A206B6120706F702E> <901FA3> <901fa>");
+        let mut bytes = Tokenizer::from("<4E6F762073686D6F7A206B6120706F702E> <901FA3> <901fa>");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("Nov shmoz ka pop."));
         assert_eq!(bytes.read_obj().unwrap(), Object::String([0x90, 0x1F, 0xA3].into()));
         assert_eq!(bytes.read_obj().unwrap(), Object::String([0x90, 0x1F, 0xA0].into()));
 
-        let mut bytes = ByteIterator::from("<61\r\n62> <61%comment\n>");
+        let mut bytes = Tokenizer::from("<61\r\n62> <61%comment\n>");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_string("ab"));
         assert!(bytes.read_obj().is_err());
     }
 
     #[test]
     fn test_read_name() {
-        let mut bytes = ByteIterator::from("/Name1 /A;Name_With-Various***Characters? /1.2 /$$ /@pattern
+        let mut bytes = Tokenizer::from("/Name1 /A;Name_With-Various***Characters? /1.2 /$$ /@pattern
             /.notdef /Lime#20Green /paired#28#29parentheses /The_Key_of_F#23_Minor /A#42");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name("Name1"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name("A;Name_With-Various***Characters?"));
@@ -376,7 +396,7 @@ are the same.) (These two strings are the same.)");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name("The_Key_of_F#_Minor"));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name("AB"));
 
-        let mut bytes = ByteIterator::from("//%\n1 /ok /invalid#00byte /#0x /#0 true");
+        let mut bytes = Tokenizer::from("//%\n1 /ok /invalid#00byte /#0x /#0 true");
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name(""));
         assert_eq!(bytes.read_obj().unwrap(), Object::new_name(""));
         assert_eq!(bytes.read_obj().unwrap(), Object::Number(Number::Int(1)));
@@ -389,7 +409,7 @@ are the same.) (These two strings are the same.)");
 
     #[test]
     fn test_read_array() {
-        let mut bytes = ByteIterator::from("[549 3.14 false (Ralph) /SomeName] [ %\n ] [false%]");
+        let mut bytes = Tokenizer::from("[549 3.14 false (Ralph) /SomeName] [ %\n ] [false%]");
         assert_eq!(bytes.read_obj().unwrap(), Object::Array([
                 Object::Number(Number::Int(549)),
                 Object::Number(Number::Real(3.14)),
