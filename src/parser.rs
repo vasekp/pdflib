606750e544b959c9900324471bd3d34c727b4772
Vašek Potoček <vasek.potocek@post.cz>
2024-12-19 09:54:41 +0100

    ByteProvider::read_line(), ::skip_past_eol()

--- src/parser.rs
+++ src/parser.rs
@@ -32,6 +32,62 @@ pub trait ByteProvider: BufRead + Seek {
             None
         }
     }
+
+    fn read_line(&mut self) -> std::io::Result<Vec<u8>> {
+        let mut line = Vec::new();
+        loop {
+            let buf = match self.fill_buf() {
+                Ok(buf) => buf,
+                Err(err) if err.kind() == std::io::ErrorKind::Interrupted => continue,
+                Err(err) => return Err(err)
+            };
+            if buf.is_empty() {
+                if line.is_empty() {
+                    return Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof));
+                } else {
+                    break;
+                }
+            }
+            match buf.iter().position(|c| *c == b'\n' || *c == b'\r') {
+                Some(pos) => {
+                    line.extend_from_slice(&buf[..pos]);
+                    let crlf = buf[pos] == b'\r' && buf.len() > pos && buf[pos + 1] == b'\n';
+                    self.consume(pos + if crlf { 2 } else { 1 });
+                    break;
+                },
+                None => {
+                    line.extend_from_slice(buf);
+                    let len = buf.len();
+                    self.consume(len);
+                }
+            }
+        }
+        Ok(line)
+    }
+
+    fn skip_past_eol(&mut self) -> std::io::Result<()> {
+        loop {
+            let buf = match self.fill_buf() {
+                Ok(buf) => buf,
+                Err(err) if err.kind() == std::io::ErrorKind::Interrupted => continue,
+                Err(err) => return Err(err)
+            };
+            if buf.is_empty() {
+                return Err(std::io::Error::from(std::io::ErrorKind::UnexpectedEof));
+            }
+            match buf.iter().position(|c| *c == b'\n' || *c == b'\r') {
+                Some(pos) => {
+                    let crlf = buf[pos] == b'\r' && buf.len() > pos && buf[pos + 1] == b'\n';
+                    self.consume(pos + if crlf { 2 } else { 1 });
+                    return Ok(());
+                },
+                None => {
+                    let len = buf.len();
+                    self.consume(len);
+                }
+            }
+        }
+    }
 }
 
 impl<T: BufRead + Seek> ByteProvider for T { }
@@ -352,6 +408,26 @@ impl<T: Into<String>> From<T> for Parser<Cursor<String>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_read_line() {
+        let mut bytes = Cursor::new("line 1\nline 2\rline 3\r\nline 4\n\rline 5");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"line 1");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"line 2");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"line 3");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"line 4");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"");
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"line 5");
+        assert!(ByteProvider::read_line(&mut bytes).is_err());
+
+        let mut bytes = Cursor::new("line 1\nline 2\rline 3\r\nline 4\n\rline 5");
+        bytes.skip_past_eol().unwrap();
+        bytes.skip_past_eol().unwrap();
+        bytes.skip_past_eol().unwrap();
+        bytes.skip_past_eol().unwrap();
+        assert_eq!(ByteProvider::read_line(&mut bytes).unwrap(), b"");
+        assert!(ByteProvider::skip_past_eol(&mut bytes).is_err());
+    }
 
     #[test]
     fn test_tokenizer() {
