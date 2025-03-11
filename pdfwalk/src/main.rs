ba52bbc5a58e7c895bcef263bf12282b00ad0db3
Vašek Potoček <vasek.potocek@post.cz>
2025-03-11 23:11:33 +0100

    PDFwalk: use less for stream viewing

--- pdfwalk/src/main.rs
+++ pdfwalk/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, Read};
+use std::io::BufReader;
 use std::fs::File;
 
 use pdflib as pdf;
@@ -31,32 +31,14 @@ fn main() -> Result<(), pdf::Error> {
                         log::error!("Not a stream object.");
                         continue;
                     };
-                    let data = reader.read_stream_data(&stm)?;
-                    let mut read = 0;
-                    let mut special = 0;
-                    let mut need_nl = true;
-                    for c in data.bytes() {
-                        let c = c?;
-                        match c {
-                            0x20..=0x7E | b'\n' => {
-                                print!("{}", c as char);
-                                read += 1;
-                                need_nl = c != b'\n';
-                            },
-                            _ => {
-                                print!("\x1B[7m<{:02x}>\x1B[0m", c);
-                                special += 1;
-                            }
-                        }
-                        if read > 1000 || special > 10 {
-                            println!("...");
-                            need_nl = false;
-                            break;
-                        }
-                    }
-                    if need_nl {
-                        println!();
-                    }
+                    let mut data = reader.read_stream_data(&stm)?;
+                    let mut cmd = std::process::Command::new("less")
+                        .stdin(std::process::Stdio::piped())
+                        .arg("-R")
+                        .spawn()?;
+                    let mut stdin = cmd.stdin.as_ref().unwrap();
+                    std::io::copy(&mut data, &mut stdin)?;
+                    cmd.wait()?;
                 }
                 _ => {}
             },
