153fbcbf5b742bba4bd9e49b64fb20b277f59a58
Vašek Potoček <vasek.potocek@post.cz>
2025-03-13 20:54:29 +0100

    Codecs: convert Names into an internal enum

--- pdflib/src/codecs/mod.rs
+++ pdflib/src/codecs/mod.rs
@@ -3,34 +3,55 @@ mod asciihex;
 
 use crate::base::*;
 use std::io::BufRead;
-use log::*;
+
+/// Supported PDF filters.
+pub enum Filter {
+    /// `/FlateDecode`
+    Flate,
+    /// `/ASCIIHexDecode`
+    AsciiHex,
+}
+
+impl TryFrom<&Name> for Filter {
+    type Error = Error;
+
+    fn try_from(name: &Name) -> Result<Filter, Error> {
+        if name == b"FlateDecode" {
+            Ok(Filter::Flate)
+        } else if name == b"ASCIIHexDecode" {
+            Ok(Filter::AsciiHex)
+        } else {
+            Err(Error::Parse("unimplemented filter"))
+        }
+    }
+}
 
 /// Wraps a `BufRead` in an adapter decoding the data according to the provided `/Filter` and 
 /// `/DecodeParms` configuration.
 ///
 /// Both of these need to be provided as fully resolved objects. Moreover, the `filter` argument
-/// needs to be provided in the form of an array of [`Name`]s. To convert a generic `Object` (which 
-/// conformantly also could be a single `/Name` or `null`) to this unified format, see 
-/// [`to_filters()`].
-pub fn decode<'a, R: BufRead + 'a>(input: R, filter: &[Name], params: Option<&Dict>) -> Box<dyn BufRead + 'a> {
+/// needs to be provided in the form of an array of [`Filter`]s. To convert a generic `Object` 
+/// to this unified format, see [`to_filters()`].
+pub fn decode<'a, R: BufRead + 'a>(input: R, filter: &[Filter], params: Option<&Dict>) -> Box<dyn BufRead + 'a> {
     match filter {
         [] => Box::new(input),
-        [name] if name == b"FlateDecode" => flate::decode(input, params.unwrap_or(&Dict::default())),
-        [name] if name == b"ASCIIHexDecode" => Box::new(asciihex::decode(input)),
+        [Filter::Flate] => flate::decode(input, params.unwrap_or(&Dict::default())),
+        [Filter::AsciiHex] => Box::new(asciihex::decode(input)),
         _ => {
-            error!("skipping unimplemented filter: {:?}", filter);
-            Box::new(input)
+            //error!("skipping unimplemented filter: {:?}", filter);
+            //Box::new(input)
+            panic!() // TODO
         }
     }
 }
 
 /// Resolve a PDF `Object` value of the `/Filter` key into the format expected by [`decode()`].
-pub fn to_filters(obj: &Object) -> Result<Vec<Name>, Error> {
+pub fn to_filters(obj: &Object) -> Result<Vec<Filter>, Error> {
     match obj {
-        Object::Name(name) => Ok(vec![name.to_owned()]),
+        Object::Name(name) => Ok(vec![name.try_into()?]),
         Object::Array(vec) => vec.iter()
             .map(|obj| match obj {
-                Object::Name(name) => Ok(name.to_owned()),
+                Object::Name(name) => name.try_into(),
                 _ => Err(Error::Parse("malformed /Filter"))
             })
             .collect::<Result<Vec<_>, _>>(),
