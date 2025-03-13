25533cbb028a2ea565f68e4dd1233fd6228452e2
Vašek Potoček <vasek.potocek@post.cz>
2025-03-13 23:25:03 +0100

    Codecs: filter chaining

--- pdflib/src/codecs/mod.rs
+++ pdflib/src/codecs/mod.rs
@@ -31,12 +31,27 @@ impl TryFrom<&Name> for Filter {
 ///
 /// The latter needs to be provided as fully resolved objects. Moreover, the `filter` argument 
 /// needs to be provided in the form of an array of [`Filter`]s.
-pub fn decode<'a, R: BufRead + 'a>(input: R, filter: &[Filter], params: Option<&Dict>) -> Box<dyn 
-BufRead + 'a> {
+pub fn decode<'a, R: BufRead + 'a>(input: R, filter: &[Filter], params: Option<&Dict>) -> Box<dyn BufRead + 'a> {
     match filter {
         [] => Box::new(input),
         [Filter::Flate] => flate::decode(input, params.unwrap_or(&Dict::default())),
         [Filter::AsciiHex] => Box::new(asciihex::decode(input)),
-        _ => panic!() // TODO
+        [_, ..] => decode(decode(input, &filter[..1], params), &filter[1..], None),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_filter_chaining() {
+        let data = "78 9c 2b 49 2d 2e 01 00 04 5d 01 c1";
+        let input = Cursor::new(data);
+        let mut output = decode(input, &[Filter::AsciiHex, Filter::Flate], None);
+        let mut data_out = String::new();
+        output.read_to_string(&mut data_out).unwrap();
+        assert_eq!(data_out, "test");
     }
 }
